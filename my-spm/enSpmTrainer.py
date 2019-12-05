fca30a0188de2748ed5660f1d0c3d46c4e36ebb5
itsmedachan <y.ishida1012@gmail.com>
2019-12-05 20:40:13 +0900

    enwiki spm

---
+++ my-spm/enSpmTrainer.py
@@ -0,0 +1,2 @@
+import sentencepiece as spm
+spm.SentencePieceTrainer.Train('--pad_id=0, --unk_id=1, --bos_id=2, --eos_id=3, --pad_piece=<pad>, --unk_piece=<unk>, --bos_piece=<s>, --eos_piece=</s>, --user_defined_symbols=<mask>, --input=enwiki_removed_doc_tag.txt, --model_prefix=enwiki --character_coverage=1.0 --vocab_size=16000, --input_sentence_size=800000, --shuffle_input_sentence=true --max_sentence_length=1000')
