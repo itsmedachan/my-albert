2ad35865be49853cec310d2fbc495647800edd71
itsmedachan <y.ishida1012@gmail.com>
2019-12-06 13:54:48 +0900

    rename trainer.py to run.py

---
+++ run.py
@@ -0,0 +1,4 @@
+from tokenizer import SentencePieceTokenizer
+
+en_tokenizer = SentencePieceTokenizer("../my-spm/enwiki.model")
+ja_tokenizer = SentencePieceTokenizer("../my-spm/jawiki.model")
--- trainer.py
+++
@@ -1,4 +0,0 @@
-from tokenizer import SentencePieceTokenizer
-
-en_tokenizer = SentencePieceTokenizer("en_path指定")
-ja_tokenizer = SentencePieceTokenizer("ja_path指定")
